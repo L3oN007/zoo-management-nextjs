c85a491c43d1877b8c46bda9d4793028da2b8a4d
Minh Nhật <89580650+L3oN007@users.noreply.github.com>
2023-10-11 14:43:58 +0700

    Update tag in manage-news-form

--- app/(role)/(routes)/staff/manage-news/components/client.tsx
+++ app/(role)/(routes)/staff/manage-news/components/client.tsx
@@ -4,23 +4,21 @@ import { Button } from '@/components/ui/button';
 import { DataTable } from '@/components/ui/data-table';
 import { Heading } from '@/components/ui/heading';
 import { Separator } from '@/components/ui/separator';
-import { Plus } from 'lucide-react';
+import { BookCopyIcon, Plus } from 'lucide-react';
 import { useRouter } from 'next/navigation';
 import { FC, useMemo } from 'react';
 import { columns } from './columns';
 
 interface ManageNewsClientProps {
 	data: any;
-
 }
 
 export const ManageNewsClient: FC<ManageNewsClientProps> = ({ data }) => {
 	const router = useRouter();
 	const uniqueTags = useMemo(() => {
-		const allTags = data.flatMap((item: any) => item.tags);
+		const allTags = data.flatMap((item: any) => item.tag);
 		return [...new Set(allTags)];
 	}, [data]) as string[];
-
 
 	const tagsData = uniqueTags.map((tag) => ({
 		label: tag,
@@ -30,14 +28,22 @@ export const ManageNewsClient: FC<ManageNewsClientProps> = ({ data }) => {
 	return (
 		<>
 			<div className='flex items-center justify-between'>
-				<Heading title={`News (${Object.keys(data).length})`} description='Manage News information in the zoo' />
+				<Heading
+					title={`News (${Object.keys(data).length})`}
+					description='Manage News information in the zoo'
+				/>
 				<Button onClick={() => router.push('/staff/manage-news/new')}>
 					<Plus className='mr-2 h-4 w-4' />
 					Add New
 				</Button>
 			</div>
 			<Separator />
-			<DataTable columns={columns} data={data} searchKey='title' filterOptions={tagsData} />
+			<DataTable
+				columns={columns}
+				data={data}
+				searchKey='title'
+				filterOptions={tagsData}
+			/>
 		</>
 	);
 };
