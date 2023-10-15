84bc32fc7fc119c8143983de41344c4afb3993cb
Minh Nhật <89580650+L3oN007@users.noreply.github.com>
2023-10-15 20:29:22 +0700

    Simple ViewDirective in Fedding Schedule

--- app/(role)/(routes)/trainer/feeding-schedule/page.tsx
+++ app/(role)/(routes)/trainer/feeding-schedule/page.tsx
@@ -2,114 +2,117 @@
 import React, { useEffect, useState } from 'react';
 import axios from 'axios';
 import {
-	ScheduleComponent,
-	Day,
-	Week,
-	Month,
-	Inject,
-	DragAndDrop,
-	Agenda,
+  ScheduleComponent,
+  Day,
+  Week,
+  Month,
+  Inject,
+  DragAndDrop,
+  Agenda,
+  ViewDirective,
+  ViewsDirective,
+  TimelineViews,
+  TimelineMonth
 } from '@syncfusion/ej2-react-schedule';
 import { registerLicense } from '@syncfusion/ej2-base';
-registerLicense(
-	'Ngo9BigBOggjHTQxAR8/V1NHaF5cXmVCf1JpRGBGfV5yd0VDalhRTnVZUj0eQnxTdEZiWX5bcXZWRmFUVUR2Ww=='
-);
+registerLicense('Ngo9BigBOggjHTQxAR8/V1NHaF5cXmVCf1JpRGBGfV5yd0VDalhRTnVZUj0eQnxTdEZiWX5bcXZWRmFUVUR2Ww==');
 
 interface Event {
-	id: string;
-	Subject: string;
-	StartTime: string;
-	EndTime: string;
-	IsAllDay: boolean;
-	ProjectId: number;
-	TaskId: number;
-	Fed: boolean;
-	IsReadonly: boolean;
+  id: string;
+  Subject: string;
+  StartTime: string;
+  EndTime: string;
+  IsAllDay: boolean;
+  ProjectId: number;
+  TaskId: number;
+  Fed: boolean;
+  IsReadonly: boolean;
 }
 
 const SchedulePage: React.FC = () => {
-	const [events, setEvents] = useState<Event[]>([]);
+  const [events, setEvents] = useState<Event[]>([]);
 
-	useEffect(() => {
-		fetchEvents();
-	}, []);
+  useEffect(() => {
+    fetchEvents();
+  }, []);
 
-	const fetchEvents = () => {
-		axios
-			.get<Event[]>('https://651d776944e393af2d59dbd7.mockapi.io/schedule')
-			.then((response) => {
-				// Update events with IsReadonly property based on the current day
-				const currentDate = new Date();
-				const updatedEvents = response.data.map((event) => ({
-					...event,
-					IsReadonly: new Date(event.StartTime) < currentDate,
-				}));
-				setEvents(updatedEvents);
-			})
-			.catch((error) => {
-				console.error(error);
-			});
-	};
+  const fetchEvents = () => {
+    axios
+      .get<Event[]>('https://651d776944e393af2d59dbd7.mockapi.io/schedule')
+      .then((response) => {
+        // Update events with IsReadonly property based on the current day
+        const currentDate = new Date();
+        const updatedEvents = response.data.map((event) => ({
+          ...event,
+          IsReadonly: new Date(event.StartTime) < currentDate
+        }));
+        setEvents(updatedEvents);
+      })
+      .catch((error) => {
+        console.error(error);
+      });
+  };
 
-	const addEvent = (newEvent: Event) => {
-		console.log(newEvent);
-		axios
-			.post<Event[]>(
-				'https://651d776944e393af2d59dbd7.mockapi.io/schedule',
-				newEvent
-			)
-			.then(() => {
-				fetchEvents();
-			})
-			.catch((error) => {
-				console.error(error);
-			});
-	};
+  const addEvent = (newEvent: Event) => {
+    console.log(newEvent);
+    axios
+      .post<Event[]>('https://651d776944e393af2d59dbd7.mockapi.io/schedule', newEvent)
+      .then(() => {
+        fetchEvents();
+      })
+      .catch((error) => {
+        console.error(error);
+      });
+  };
 
-	const updateEvent = (updatedEvent: Event) => {
-		axios
-			.put(
-				`https://651d776944e393af2d59dbd7.mockapi.io/schedule/${updatedEvent.id}`,
-				updatedEvent
-			)
-			.then(() => {
-				fetchEvents();
-			})
-			.catch((error) => {
-				console.error(error);
-			});
-	};
+  const updateEvent = (updatedEvent: Event) => {
+    axios
+      .put(`https://651d776944e393af2d59dbd7.mockapi.io/schedule/${updatedEvent.id}`, updatedEvent)
+      .then(() => {
+        fetchEvents();
+      })
+      .catch((error) => {
+        console.error(error);
+      });
+  };
 
-	const deleteEvent = (eventId: string) => {
-		axios
-			.delete(`https://651d776944e393af2d59dbd7.mockapi.io/schedule/${eventId}`)
-			.then(() => {
-				fetchEvents();
-			})
-			.catch((error) => {
-				console.error(error);
-			});
-	};
+  const deleteEvent = (eventId: string) => {
+    axios
+      .delete(`https://651d776944e393af2d59dbd7.mockapi.io/schedule/${eventId}`)
+      .then(() => {
+        fetchEvents();
+      })
+      .catch((error) => {
+        console.error(error);
+      });
+  };
 
-	return (
-		<div>
-			<ScheduleComponent
-				width='100%'
-				height='550px'
-				eventSettings={{ dataSource: events }}
-				actionBegin={(args) => {
-					if (args.requestType === 'eventCreate') {
-						addEvent(args.data as Event);
-					} else if (args.requestType === 'eventChange') {
-						updateEvent(args.data as Event);
-					} else if (args.requestType === 'eventRemove') {
-						deleteEvent((args.data[0] as Event).id);
-					}
-				}}>
-				<Inject services={[Day, Week, Month, DragAndDrop, Agenda]} />
-			</ScheduleComponent>
-		</div>
-	);
+  return (
+    <div>
+      <ScheduleComponent
+        width="100%"
+        height="550px"
+        eventSettings={{ dataSource: events }}
+        actionBegin={(args) => {
+          if (args.requestType === 'eventCreate') {
+            addEvent(args.data as Event);
+          } else if (args.requestType === 'eventChange') {
+            updateEvent(args.data as Event);
+          } else if (args.requestType === 'eventRemove') {
+            deleteEvent((args.data[0] as Event).id);
+          }
+        }}
+      >
+        <ViewsDirective>
+          <ViewDirective option="Day" startHour="06:00" endHour="19:00"></ViewDirective>
+          <ViewDirective option="Week" isSelected={true}></ViewDirective>
+          <ViewDirective option="TimelineDay"></ViewDirective>
+          <ViewDirective option="TimelineMonth"></ViewDirective>
+        </ViewsDirective>
+        <Inject services={[Day, Week, Month, DragAndDrop, Agenda, TimelineViews, TimelineMonth]} />
+      </ScheduleComponent>
+    </div>
+  );
 };
 
 export default SchedulePage;
