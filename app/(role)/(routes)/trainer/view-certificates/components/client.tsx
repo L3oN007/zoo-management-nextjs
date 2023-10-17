12297e4e4572e374931b74617cfa72d2312f0c9f
thanhvann <tvtvan1407@gmail.com>
2023-10-17 13:08:58 +0700

    UI view certificates of trainer

---
+++ app/(role)/(routes)/trainer/view-certificates/components/client.tsx
@@ -0,0 +1,35 @@
+"use client"
+
+import { Button } from "@/components/ui/button"
+import { DataTable } from "@/components/ui/data-table"
+import { Heading } from "@/components/ui/heading"
+import { Separator } from "@/components/ui/separator"
+import { Plus } from "lucide-react"
+import { useParams, useRouter } from "next/navigation"
+import { FC, useState } from "react"
+import { columns } from "./columns"
+
+
+interface ManageCertificateClientProps {
+    data: any
+}
+
+export const ManageCertificateClient: FC<ManageCertificateClientProps> = (data) => {
+    const router = useRouter();
+
+    return (    
+        <>
+            <div className="flex items-center justify-between">
+                <Heading title={`Certificate (${Object.keys(data.data).length})`} description="List certificate" />
+
+                <Button onClick={() => router.push("/trainer/manage-certificates/new")}>
+                    <Plus className="mr-2 h-4 w-4" />
+                    Add New
+                </Button>
+            </div>
+            <Separator />
+            <DataTable columns={columns} data={data.data} searchKey="cerName" filterOptions={null as any} />
+
+        </>
+    )
+}
