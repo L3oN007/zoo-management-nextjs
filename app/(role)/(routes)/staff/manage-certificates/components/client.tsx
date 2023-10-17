ae0ad86129ecdfe0e707c8df31f718f0d02b609f
thanhvann <tvtvan1407@gmail.com>
2023-10-17 13:06:08 +0700

    add UI manage certificates of staff

---
+++ app/(role)/(routes)/staff/manage-certificates/components/client.tsx
@@ -0,0 +1,45 @@
+"use client"
+
+import { Button } from "@/components/ui/button"
+import { DataTable } from "@/components/ui/data-table"
+import { Heading } from "@/components/ui/heading"
+import { Separator } from "@/components/ui/separator"
+import { Plus } from "lucide-react"
+import { useParams, useRouter } from "next/navigation"
+import { FC, useState } from "react"
+import { columns } from "./columns"
+import { empColumns } from "./empColumns"
+
+
+interface ManageCertificateClientProps {
+    data: any,
+    empCer: any
+
+}
+
+export const ManageCertificateClient: FC<ManageCertificateClientProps> = ({data, empCer}) => {
+    const router = useRouter();
+
+   
+
+    return (    
+        <>
+            <div className="flex items-center justify-between">
+                <Heading title={`Certificate (${Object.keys(data).length})`} description="Manage Certificate information of trainer" />
+
+                <Button onClick={() => router.push("/trainer/manage-certificates/new")}>
+                    <Plus className="mr-2 h-4 w-4" />
+                    Add New
+                </Button>
+            </div>
+            <Separator />
+            <div className="flex justify-around">
+            <DataTable key="certificate" columns={columns} data={data} searchKey="cerName" filterOptions={null as any} />
+            <div className="ml-4">
+            <DataTable key="empCertificate" columns={empColumns} data={empCer} searchKey={"cerCode"}  filterOptions={null as any} />
+
+            </div>
+            </div>
+        </>
+    )
+}
---
+++ app/(role)/(routes)/staff/manage-certificates/components/columns.tsx
@@ -0,0 +1,56 @@
+'use client';
+
+import { Avatar, AvatarFallback, AvatarImage } from '@/components/ui/avatar';
+import { Badge } from '@/components/ui/badge';
+import { Button } from '@/components/ui/button';
+import { ColumnDef } from '@tanstack/react-table';
+import { ArrowUpDown, MoreHorizontal } from "lucide-react"
+import { CellAction } from './cell-action';
+import { DataTableRowActions } from '@/components/data-table/data-table-row-actions';
+import { AreaObj } from '@/app/models/area';
+
+export type CertificateColumn = {
+    cerCode: string;
+    cerName: string;
+    level: string;
+    trainingInsti: string;
+};
+
+export const columns: ColumnDef<CertificateColumn>[] = [
+    {
+        accessorKey: 'cerCode',
+        header: 'Certificate Code',
+
+    },
+    {
+        accessorKey: 'cerName',
+        header: ({ column }) => {
+            return (
+                <Button
+                    variant="ghost"
+                    onClick={() => column.toggleSorting(column.getIsSorted() === "asc")}
+                >
+                    Certificate Name
+                    <ArrowUpDown className="ml-2 h-4 w-4" />
+                </Button>
+            )
+        },
+    },
+    {
+        accessorKey: 'level',
+        header: 'Level',
+
+    },
+
+    {
+        accessorKey: 'trainingInsti',
+        header: 'Training Institution',
+
+    },
+    {
+        id: "actions",
+        cell: ({ row }) => <CellAction data={row.original} />
+    },
+];
+
+
