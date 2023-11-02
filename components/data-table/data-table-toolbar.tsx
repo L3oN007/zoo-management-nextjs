3a5c1154512efbe5b5d47cd5236fcd4c94f73fd1
Minh Nhật <89580650+L3oN007@users.noreply.github.com>
2023-11-02 21:26:49 +0700

    Done filter for cage (filter area name)

--- components/data-table/data-table-toolbar.tsx
+++ components/data-table/data-table-toolbar.tsx
@@ -1,13 +1,13 @@
-"use client";
+'use client';
 
-import { Cross2Icon } from "@radix-ui/react-icons";
-import { Table } from "@tanstack/react-table";
+import { Cross2Icon } from '@radix-ui/react-icons';
+import { Table } from '@tanstack/react-table';
 
-import { Button } from "@/components/ui/button";
-import { Input } from "@/components/ui/input";
-import { DataTableViewOptions } from "./data-table-view-options";
+import { Button } from '@/components/ui/button';
+import { Input } from '@/components/ui/input';
+import { DataTableViewOptions } from './data-table-view-options';
 
-import { DataTableFacetedFilter } from "./data-table-faceted-filter";
+import { DataTableFacetedFilter } from './data-table-faceted-filter';
 
 interface DataTableToolbarProps<TData> {
   table: Table<TData>;
@@ -19,11 +19,7 @@ interface DataTableToolbarProps<TData> {
   }[];
 }
 
-export function DataTableToolbar<TData>({
-  table,
-  searchKey,
-  filterOptions,
-}: DataTableToolbarProps<TData>) {
+export function DataTableToolbar<TData>({ table, searchKey, filterOptions }: DataTableToolbarProps<TData>) {
   const isFiltered = table.getState().columnFilters.length > 0;
 
   return (
@@ -31,39 +27,27 @@ export function DataTableToolbar<TData>({
       <div className="flex flex-1 items-center space-x-2">
         <Input
           placeholder="Search"
-          value={(table.getColumn(searchKey)?.getFilterValue() as string) ?? ""}
-          onChange={(event) =>
-            table.getColumn(searchKey)?.setFilterValue(event.target.value)
-          }
+          value={(table.getColumn(searchKey)?.getFilterValue() as string) ?? ''}
+          onChange={(event) => table.getColumn(searchKey)?.setFilterValue(event.target.value)}
           className="h-8 w-[150px] lg:w-[250px]"
         />
-        {table.getColumn("employeeStatus") && (
-          <DataTableFacetedFilter
-            column={table.getColumn("employeeStatus")}
-            title="Status"
-            options={filterOptions}
-          />
+      </div>
+      <div className="flex space-x-2">
+        {table.getColumn('employeeStatus') && (
+          <DataTableFacetedFilter column={table.getColumn('employeeStatus')} title="Status" options={filterOptions} />
         )}
-        {table.getColumn("tag") && (
-          <DataTableFacetedFilter
-            column={table.getColumn("tag")}
-            title="Tag"
-            options={filterOptions}
-          />
+        {table.getColumn('area_areaName') && (
+          <DataTableFacetedFilter column={table.getColumn('area_areaName')} title="Area" options={filterOptions} />
         )}
-
         {isFiltered && (
-          <Button
-            variant="ghost"
-            onClick={() => table.resetColumnFilters()}
-            className="h-8 px-2 lg:px-3"
-          >
+          <Button variant="ghost" onClick={() => table.resetColumnFilters()} className="h-8 px-2 lg:px-3 ">
             Reset
             <Cross2Icon className="ml-2 h-4 w-4" />
           </Button>
         )}
       </div>
-      <DataTableViewOptions table={table} />
+
+      {/* <DataTableViewOptions table={table} /> */}
     </div>
   );
 }
