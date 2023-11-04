8c384b7a86c59080ab492498996262c6b7b6bdff
thanhvann <tvtvan1407@gmail.com>
2023-11-04 23:37:27 +0700

    fix UI food

--- app/(role)/(routes)/staff/manage-foods/page.tsx
+++ app/(role)/(routes)/staff/manage-foods/page.tsx
@@ -46,7 +46,7 @@ const ManageFoodPage = async () => {
     return (
       <div className="flex-col">
         <div className="flex-1 space-y-4 p-8 pt-6">
-          <ManageFoodClient data={importData} food={food} />
+          {/* <ManageFoodClient data={importData} food={food} /> */}
         </div>
       </div>
     );
