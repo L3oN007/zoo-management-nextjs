ab45c48bc0d369cd61d0fbf0b24419d40a543814
Minh Nhật <89580650+L3oN007@users.noreply.github.com>
2023-11-06 21:23:34 +0700

    New login page

--- app/page.tsx
+++ app/page.tsx
@@ -2,64 +2,25 @@ import { Metadata } from 'next';
 import Link from 'next/link';
 
 import { UserAuthForm } from './user-auth-form';
+import Image from 'next/image';
 
 export const metadata: Metadata = {
-	title: 'Authentication',
-	description: 'Authentication forms built using the components.',
+  title: 'Authentication',
+  description: 'Authentication forms built using the components.'
 };
 
 export default function AuthenticationPage() {
-	return (
-		<>
-			<div className='container relative hidden h-full flex-col items-center justify-center md:grid lg:max-w-none lg:grid-cols-2 lg:px-0'>
-				<div className='relative hidden h-full flex-col bg-muted p-10 text-white dark:border-r lg:flex'>
-					<div className='absolute inset-0 bg-zinc-900' />
-					<div className='relative z-20 flex items-center text-lg font-medium'>
-						<svg
-							xmlns='http://www.w3.org/2000/svg'
-							viewBox='0 0 24 24'
-							fill='none'
-							stroke='currentColor'
-							strokeWidth='2'
-							strokeLinecap='round'
-							strokeLinejoin='round'
-							className='mr-2 h-6 w-6'
-						>
-							<path d='M15 6v12a3 3 0 1 0 3-3H6a3 3 0 1 0 3 3V6a3 3 0 1 0-3 3h12a3 3 0 1 0-3-3' />
-						</svg>
-						Acme Inc
-					</div>
-					<div className='relative z-20 mt-auto'>
-						<blockquote className='space-y-2'>
-							<p className='text-lg'>
-								&ldquo;This library has saved me countless hours of work and helped me deliver stunning designs to my clients faster than ever
-								before.&rdquo;
-							</p>
-							<footer className='text-sm'>Sofia Davis</footer>
-						</blockquote>
-					</div>
-				</div>
-				<div className='lg:p-8'>
-					<div className='mx-auto flex w-full flex-col justify-center space-y-6 sm:w-[350px]'>
-						<div className='flex flex-col space-y-2 text-center'>
-							<h1 className='text-2xl font-semibold tracking-tight'>Login to system</h1>
-							<p className='text-sm text-muted-foreground'>Enter Username and Password to login</p>
-						</div>
-						<UserAuthForm />
-						<p className='px-8 text-center text-sm text-muted-foreground'>
-							By clicking continue, you agree to our{' '}
-							<Link href='/terms' className='underline underline-offset-4 hover:text-primary'>
-								Terms of Service
-							</Link>{' '}
-							and{' '}
-							<Link href='/privacy' className='underline underline-offset-4 hover:text-primary'>
-								Privacy Policy
-							</Link>
-							.
-						</p>
-					</div>
-				</div>
-			</div>
-		</>
-	);
+  return (
+    <>
+      <div className="flex min-h-full flex-1 flex-col justify-center px-6 py-12 lg:px-8">
+        <div className="sm:mx-auto sm:w-full sm:max-w-sm">
+          <Image className="mx-auto " src="/images/logo-light.svg" alt="logo" width={160} height={120} />
+          <h2 className="mt-10 text-center text-2xl font-bold leading-9 tracking-tight text-gray-900">
+            Sign in to your account
+          </h2>
+        </div>
+        <UserAuthForm />
+      </div>
+    </>
+  );
 }
