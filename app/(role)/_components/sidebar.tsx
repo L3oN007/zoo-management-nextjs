bef918f6621a39a576fc1af779af802bbfeefc93
Long Vu <vulongbinhduong@gmail.com>
2023-11-09 20:55:36 +0700

    hotfix logo at sidebar.tsx

--- app/(role)/_components/sidebar.tsx
+++ app/(role)/_components/sidebar.tsx
@@ -5,7 +5,7 @@ import { SidebarRoutes } from './sidebar-routes';
 export const Sidebar = () => {
   return (
     <div className="h-full border-r flex flex-col overflow-y-auto bg-white shadow-sm ">
-      <div className="py-4 px-8 ">
+      <div className="py-4 px-10 ">
         <Logo />
       </div>
       <div className="flex flex-col w-full h-full">
