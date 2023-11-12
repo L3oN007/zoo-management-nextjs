52164c8bd7ca9577a367e000c78cc5e7234d592f
Minh Nhật <89580650+L3oN007@users.noreply.github.com>
2023-11-12 17:44:29 +0700

    update overview in staff,admin

--- app/(role)/(routes)/admin/components/overview.tsx
+++ app/(role)/(routes)/admin/components/overview.tsx
@@ -1,78 +1,85 @@
-"use client"
+'use client';
 
-import { Bar, BarChart, ResponsiveContainer, XAxis, YAxis } from "recharts"
+import { useEffect, useState } from 'react';
+import { Bar, BarChart, ResponsiveContainer, XAxis, YAxis } from 'recharts';
 
-const data = [
-    {
-        name: "Jan",
-        total: Math.floor(Math.random() * 5000) + 1000,
-    },
-    {
-        name: "Feb",
-        total: Math.floor(Math.random() * 5000) + 1000,
-    },
-    {
-        name: "Mar",
-        total: Math.floor(Math.random() * 5000) + 1000,
-    },
-    {
-        name: "Apr",
-        total: Math.floor(Math.random() * 5000) + 1000,
-    },
-    {
-        name: "May",
-        total: Math.floor(Math.random() * 5000) + 1000,
-    },
-    {
-        name: "Jun",
-        total: Math.floor(Math.random() * 5000) + 1000,
-    },
-    {
-        name: "Jul",
-        total: Math.floor(Math.random() * 5000) + 1000,
-    },
-    {
-        name: "Aug",
-        total: Math.floor(Math.random() * 5000) + 1000,
-    },
-    {
-        name: "Sep",
-        total: Math.floor(Math.random() * 5000) + 1000,
-    },
-    {
-        name: "Oct",
-        total: Math.floor(Math.random() * 5000) + 1000,
-    },
-    {
-        name: "Nov",
-        total: Math.floor(Math.random() * 5000) + 1000,
-    },
-    {
-        name: "Dec",
-        total: Math.floor(Math.random() * 5000) + 1000,
-    },
-]
+interface OverviewProps {
+  dataReal: any;
+}
 
-export function Overview() {
-    return (
-        <ResponsiveContainer width="100%" height={350}>
-            <BarChart data={data}>
-                <XAxis
-                    dataKey="name"
-                    stroke="#888888"
-                    fontSize={12}
-                    tickLine={false}
-                    axisLine={false}
-                />
-                <YAxis
-                    stroke="#888888"
-                    fontSize={12}
-                    tickLine={false}
-                    axisLine={false}
-                    tickFormatter={(value) => `$${value}`}
-                />
-                <Bar dataKey="total" fill="#adfa1d" radius={[4, 4, 0, 0]} />
-            </BarChart>
-        </ResponsiveContainer>
-    )
+export function getMonthName(monthNumber: number): string {
+  const months = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'];
+
+  if (monthNumber >= 1 && monthNumber <= 12) {
+    return months[monthNumber - 1];
+  }
+
+  return 'Unknown'; // Handle invalid month numbers as needed
+}
+
+export function Overview({ dataReal }: OverviewProps) {
+  const dataWithMonth = dataReal?.map((item: { purchaseDate: Date }) => ({
+    ...item,
+    month: new Date(item.purchaseDate).getMonth() + 1
+  }));
+
+  const [revenueData, setRevenueData] = useState<any>();
+
+  const calculateMonthlyRevenue = () => {
+    if (!dataWithMonth) {
+      console.warn('DataReal is null or empty.');
+      return;
+    }
+
+    interface Transaction {
+      month: number;
+      totalPrice: number;
+    }
+
+    try {
+      const monthlyRevenue: { [key: string]: number } = dataWithMonth.reduce(
+        (acc: { [key: string]: number }, transaction: Transaction) => {
+          const monthName = getMonthName(transaction.month);
+          if (!acc[monthName]) {
+            acc[monthName] = 0;
+          }
+          acc[monthName] += transaction.totalPrice;
+          return acc;
+        },
+        {}
+      );
+
+      const revenueArray = Object.entries(monthlyRevenue).map(([month, revenue]) => ({
+        month,
+        revenue
+      }));
+
+      console.log(revenueArray);
+
+      setRevenueData(revenueArray);
+    } catch (error) {
+      console.error('An error occurred while calculating monthly revenue:', error);
+      // Handle the error or set a default value for revenueData here.
+    }
+  };
+
+  useEffect(() => {
+    calculateMonthlyRevenue();
+  }, []);
+
+  return (
+    <ResponsiveContainer width="100%" height={350}>
+      <BarChart data={revenueData || []}>
+        <XAxis dataKey="month" stroke="#888888" fontSize={12} tickLine={false} axisLine={false} />
+        <YAxis
+          stroke="#888888"
+          fontSize={12}
+          tickLine={false}
+          axisLine={false}
+          tickFormatter={(value) => `${value / 1000000}`}
+        />
+        <Bar dataKey="revenue" fill="#adfa1d" radius={[4, 4, 0, 0]} />
+      </BarChart>
+    </ResponsiveContainer>
+  );
 }
--- app/(role)/(routes)/admin/page.tsx
+++ app/(role)/(routes)/admin/page.tsx
@@ -1,10 +1,10 @@
 import { Metadata } from 'next';
 
-import { Button } from '@/components/ui/button';
 import { Card, CardContent, CardDescription, CardHeader, CardTitle } from '@/components/ui/card';
 
 import { options } from '@/app/api/auth/[...nextauth]/options';
 import { Heading } from '@/components/ui/heading';
+import axios from 'axios';
 import { getServerSession } from 'next-auth';
 import { Overview } from './components/overview';
 
@@ -13,56 +13,76 @@ export const metadata: Metadata = {
   description: 'Example dashboard app built using the components.'
 };
 
+function getMonthName(monthNumber: number) {
+  const months = [
+    'January',
+    'February',
+    'March',
+    'April',
+    'May',
+    'June',
+    'July',
+    'August',
+    'September',
+    'October',
+    'November',
+    'December'
+  ];
+
+  if (monthNumber >= 1 && monthNumber <= 12) {
+    return months[monthNumber - 1];
+  } else {
+    return 'Invalid month number';
+  }
+}
+
 export default async function DashboardPage() {
   const session = await getServerSession(options);
+  const transUrl = process.env.NEXT_PUBLIC_API_LOAD_TRANSACTIONS!;
+  const orderDetailUrl = process.env.NEXT_PUBLIC_API_LOAD_ORDERDETAIL!;
+
+  let totalRevenue = 0;
+  let curTickets = 0;
+  let totalQuantityTickets = 0;
+  let transData = null;
+
+  try {
+    const transResponse = await axios.get(transUrl);
+    transData = transResponse.data;
+    console.log(transData);
+
+    transData.forEach((element: any) => {
+      totalRevenue += element.totalPrice;
+
+      if (new Date(element.purchaseDate).getMonth() + 1 === new Date().getMonth() + 1) {
+        element.order.orderDetails.forEach((od: { quantity: number }) => {
+          curTickets += od.quantity;
+        });
+      }
+    });
+
+    const odResponse = await axios.get(orderDetailUrl);
+    let odData = odResponse.data;
+
+    odData.forEach((element: any) => {
+      totalQuantityTickets += element.quantity;
+    });
+  } catch (error) {
+    // Handle the error here, you can log it or take any other necessary action.
+    console.error('An error occurred:', error);
+  }
+
   return (
     <>
       <div className="hidden flex-col md:flex">
         <div className="flex-1 space-y-4 p-8 pt-6">
           <div className="flex items-center justify-between space-y-2">
             <Heading title="Dashboard" description={`Welcome back ${session?.user.fullName}`} />
-            <div className="flex items-center space-x-2">
-              <Button>Download</Button>
-            </div>
           </div>
-          <div className="grid gap-4 md:grid-cols-2 lg:grid-cols-4">
-            <Card>
-              <div className="flex">
-                <article className="flex-1 bg-white p-6">
-                  <div>
-                    <p className="text-sm text-gray-500">Profit</p>
-
-                    <p className="text-2xl font-medium text-gray-900">$240.94</p>
-                  </div>
-
-                  <div className="mt-1 flex gap-1 text-green-600">
-                    <svg
-                      xmlns="http://www.w3.org/2000/svg"
-                      className="h-4 w-4"
-                      fill="none"
-                      viewBox="0 0 24 24"
-                      stroke="currentColor"
-                    >
-                      <path
-                        strokeLinecap="round"
-                        strokeLinejoin="round"
-                        strokeWidth="2"
-                        d="M13 7h8m0 0v8m0-8l-8 8-4-4-6 6"
-                      />
-                    </svg>
-
-                    <p className="flex gap-2 text-xs">
-                      <span className="font-medium"> 67.81% </span>
-
-                      <span className="text-gray-500"> Since last week </span>
-                    </p>
-                  </div>
-                </article>
-              </div>
-            </Card>
+          <div className="grid gap-4 md:grid-cols-2 lg:grid-cols-3">
             <Card>
               <CardHeader className="flex flex-row items-center justify-between space-y-0 pb-2">
-                <CardTitle className="text-sm font-medium">Subscriptions</CardTitle>
+                <CardTitle className="text-sm font-medium">Total Revenue of {new Date().getFullYear()}</CardTitle>
                 <svg
                   xmlns="http://www.w3.org/2000/svg"
                   viewBox="0 0 24 24"
@@ -73,19 +93,46 @@ export default async function DashboardPage() {
                   strokeWidth="2"
                   className="h-4 w-4 text-muted-foreground"
                 >
-                  <path d="M16 21v-2a4 4 0 0 0-4-4H6a4 4 0 0 0-4 4v2" />
-                  <circle cx="9" cy="7" r="4" />
-                  <path d="M22 21v-2a4 4 0 0 0-3-3.87M16 3.13a4 4 0 0 1 0 7.75" />
+                  <path d="M12 2v20M17 5H9.5a3.5 3.5 0 0 0 0 7h5a3.5 3.5 0 0 1 0 7H6" />
                 </svg>
               </CardHeader>
               <CardContent>
-                <div className="text-2xl font-bold">+2350</div>
-                <p className="text-xs text-muted-foreground">+180.1% from last month</p>
+                <div className="text-2xl font-bold">
+                  {totalRevenue.toLocaleString('vi', {
+                    style: 'currency',
+                    currency: 'VND'
+                  })}
+                </div>
               </CardContent>
             </Card>
             <Card>
               <CardHeader className="flex flex-row items-center justify-between space-y-0 pb-2">
-                <CardTitle className="text-sm font-medium">Sales</CardTitle>
+                <CardTitle className="text-sm font-medium">Total Revenue of {new Date().getFullYear()}</CardTitle>
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  viewBox="0 0 24 24"
+                  fill="none"
+                  stroke="currentColor"
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  strokeWidth="2"
+                  className="h-4 w-4 text-muted-foreground"
+                >
+                  <path d="M12 2v20M17 5H9.5a3.5 3.5 0 0 0 0 7h5a3.5 3.5 0 0 1 0 7H6" />
+                </svg>
+              </CardHeader>
+              <CardContent>
+                <div className="text-2xl font-bold">
+                  {totalRevenue.toLocaleString('vi', {
+                    style: 'currency',
+                    currency: 'VND'
+                  })}
+                </div>
+              </CardContent>
+            </Card>
+            <Card>
+              <CardHeader className="flex flex-row items-center justify-between space-y-0 pb-2">
+                <CardTitle className="text-sm font-medium">Sales in of {new Date().getFullYear()}</CardTitle>
                 <svg
                   xmlns="http://www.w3.org/2000/svg"
                   viewBox="0 0 24 24"
@@ -101,47 +148,19 @@ export default async function DashboardPage() {
                 </svg>
               </CardHeader>
               <CardContent>
-                <div className="text-2xl font-bold">+12,234</div>
-                <p className="text-xs text-muted-foreground">+19% from last month</p>
-              </CardContent>
-            </Card>
-            <Card>
-              <CardHeader className="flex flex-row items-center justify-between space-y-0 pb-2">
-                <CardTitle className="text-sm font-medium">Active Now</CardTitle>
-                <svg
-                  xmlns="http://www.w3.org/2000/svg"
-                  viewBox="0 0 24 24"
-                  fill="none"
-                  stroke="currentColor"
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                  strokeWidth="2"
-                  className="h-4 w-4 text-muted-foreground"
-                >
-                  <path d="M22 12h-4l-3 9L9 3l-3 9H2" />
-                </svg>
-              </CardHeader>
-              <CardContent>
-                <div className="text-2xl font-bold">+573</div>
-                <p className="text-xs text-muted-foreground">+201 since last hour</p>
+                <div className="text-2xl font-bold">{totalQuantityTickets} sold tickets</div>
               </CardContent>
             </Card>
           </div>
           <div className="grid gap-4 md:grid-cols-2 lg:grid-cols-7">
-            <Card className="col-span-4">
+            <Card className="col-span-full">
               <CardHeader>
-                <CardTitle>Overview</CardTitle>
+                <CardTitle>Overview Revenue Of {new Date().getFullYear()}</CardTitle>
+                <CardDescription>Millions per month</CardDescription>
               </CardHeader>
               <CardContent className="pl-2">
-                <Overview />
+                <Overview dataReal={transData} />
               </CardContent>
-            </Card>
-            <Card className="col-span-3">
-              <CardHeader>
-                <CardTitle>Recent Sales</CardTitle>
-                <CardDescription>You made 265 sales this month.</CardDescription>
-              </CardHeader>
-              <CardContent></CardContent>
             </Card>
           </div>
         </div>
